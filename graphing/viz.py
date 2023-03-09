e381ae9a9462262e63d349632c807150c6019fa0
Vincent Ferrante <83679092+vincejferrante@users.noreply.github.com>
2023-03-08 19:17:11 -0800

    Create viz.py

---
+++ graphing/viz.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+from RandomWalk import RandomWalk
+
+# Make a random walk
+while True:
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', cmap=plt.cm.Blues, edgecolors='none', s=100)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        print("good bye")
+        break
