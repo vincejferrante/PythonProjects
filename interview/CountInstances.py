a44e5a9e8544a712f550e50372634858f8e3ece9
Vincent Ferrante <83679092+vincejferrante@users.noreply.github.com>
2023-04-23 08:41:14 -0700

    Delete interview directory

--- interview/CountInstances.py
+++
@@ -1,4 +0,0 @@
-# Create a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string.
-
-def char_count(txt1, txt2):
-	return txt2.count(txt1)
